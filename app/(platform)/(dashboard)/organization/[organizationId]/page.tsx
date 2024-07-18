d7d1dbbe07ebb200f13fd6386766495f6014e37b
ouahlineomaima <omaimaouahline@gmail.com>
2024-07-18 23:49:07 +0100

    Setting up prisma and testing server actions

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { db } from "@/lib/db";
 
 const OrganizationIdPage = () => {
 
+async function create(formData: FormData){
+  "use server";
+
+  const title = formData.get("title") as string
+  
+  await db.board.create({
+    data: {
+      title
+    }
+  })
+}
+
   return (
-    <div>org id</div>
+    <div >
+      <form action={create}>
+        <input id='title' name='title' required placeholder='Enter board title' className='border-black border p-1' type="text" />
+      </form>
+    </div>
   )
 }
 
---
+++ lib/db.ts
@@ -0,0 +1,9 @@
+import {PrismaClient} from "@prisma/client"
+
+declare global{
+    var prisma: PrismaClient | undefined;
+}
+// we do that the prevent hot realoding from reassigning prisma client to db when it hot realoads since the global is excluded from hot reload
+export const db = globalThis.prisma || new PrismaClient();
+
+if(process.env.NODE_ENV !== "production") globalThis.prisma = db;
