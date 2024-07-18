0cddb37e499ac052d64ce0fea1dcccb16dec74b7
ouahlineomaima <omaimaouahline@gmail.com>
2024-07-19 00:10:37 +0100

    Displaying borads and deleting them in real time

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,24 +1,26 @@
+import { create } from "@/actions/create-board";
+import { Button } from "@/components/ui/button";
 import { db } from "@/lib/db";
+import Board from "./board";
 
-const OrganizationIdPage = () => {
+const OrganizationIdPage = async () => {
 
-async function create(formData: FormData){
-  "use server";
+  const boards = await db.board.findMany();
 
-  const title = formData.get("title") as string
-  
-  await db.board.create({
-    data: {
-      title
-    }
-  })
-}
 
   return (
-    <div >
+    <div className="flex flex-col space-y-4">
       <form action={create}>
         <input id='title' name='title' required placeholder='Enter board title' className='border-black border p-1' type="text" />
+        <Button type="submit">
+          Submit
+        </Button>
       </form>
+      <div className="space-y-2">
+        {boards.map((board)=>(
+          <Board key={board.id} title={board.title} id={board.id}/>
+        ))}
+      </div>
     </div>
   )
 }
