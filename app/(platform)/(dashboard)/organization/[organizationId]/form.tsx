cafe9422e9f70bfaa450d3ab723ce5f8d76044a2
ouahlineomaima <omaimaouahline@gmail.com>
2024-07-19 13:09:13 +0100

    Abstracting server actions

--- app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
@@ -1,21 +1,34 @@
 "use client";
 
-import { create } from '@/actions/create-board';
+import { createBoard } from '@/actions/create-board/index';
 import { Button } from '@/components/ui/button';
 import React from 'react'
 import { useFormState } from 'react-dom';
 import FormInput from './form-input';
 import FormButton from './form-button';
+import { useAction } from '@/hooks/use-action';
 
 const Form = () => {
-    const initialState = {message: null, errors: {}}
-    const [state, dispatch] = useFormState(create, initialState);
+  const {execute, fieldErrors} = useAction(createBoard, {
+    onSuccess: (data) =>{
+      console.log(data, "success")
+    },
+    onError: (error)=>{
+      console.error(error)
+    }
+  })
+
+  const onSubmit = (formData: FormData)=>{
+    const title = formData.get("title") as string;
+
+    execute({title})
+  }
   return (
     <div className="flex flex-col space-y-4">
-      <form action={dispatch}>
+      <form action={onSubmit}>
         <div className='flex flex-col space-y-2'>
 
-        <FormInput errors={state?.errors}/>
+        <FormInput errors={fieldErrors}/>
         </div>
         <FormButton/>
       </form>
