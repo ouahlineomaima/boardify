803739972594ae3db4dd10be0c736572b755548e
ouahlineomaima <omaimaouahline@gmail.com>
2024-10-09 19:21:47 +0100

    integrate drag & drop

--- app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
@@ -1,16 +1,27 @@
 "use client";
 
 import { Card } from "@prisma/client";
+import { Draggable } from "@hello-pangea/dnd";
 
-interface CardItemProps{
-    index: number
-    data: Card;
+interface CardItemProps {
+  index: number;
+  data: Card;
 }
 
-export const CardItem = ({data, index}: CardItemProps)=>{
-    return (
-        <div role="button" className="truncate border-2 border-transparent hover:border-black py-2 px-3 ttext-sm bg-white rounded-sm shadow-sm">
-            {data.title}
-            </div>
-    )
-}
+export const CardItem = ({ data, index }: CardItemProps) => {
+  return (
+    <Draggable draggableId={data.id} index={index}>
+      {(provided) => (
+        <div
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          ref={provided.innerRef}
+          role="button"
+          className="truncate border-2 border-transparent hover:border-black py-2 px-3 ttext-sm bg-white rounded-sm shadow-sm"
+        >
+          {data.title}
+        </div>
+      )}
+    </Draggable>
+  );
+};
