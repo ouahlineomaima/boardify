fbdfd2794b3385ffc66dcdff6b7956e0cc07c39d
ouahlineomaima <omaimaouahline@gmail.com>
2024-10-09 21:17:53 +0100

    Card modal with update title

--- app/(platform)/layout.tsx
+++ app/(platform)/layout.tsx
@@ -1,17 +1,18 @@
-import { ClerkProvider } from '@clerk/nextjs'
-import {Toaster} from "sonner"
+import { ModalProvider } from "@/components/providers/modal-provider";
+import { QueryProvider } from "@/components/providers/query-provider";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "sonner";
 
-const PlatformLayout = ({
-    children
-}: {
-    children: React.ReactNode
-}) => {
+const PlatformLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <ClerkProvider>
-      <Toaster/>
+      <QueryProvider>
+        <Toaster />
+        <ModalProvider />
         {children}
+      </QueryProvider>
     </ClerkProvider>
-  )
-}
+  );
+};
 
-export default PlatformLayout
+export default PlatformLayout;
